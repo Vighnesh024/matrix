eb3051529aff1ac016382f7a4ae93e6620ca0c19
Vighnesh024 <kakashikakadu024@gmail.com>
2025-05-18 16:20:30 +0530

    Auto commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar"; // make sure this exists
-import Profile from "./pages/Profile"; // make sure this exists
+import Navbar from "./components/Navbar";
+import Profile from "./pages/Profile";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import ProtectedRoute from "./components/ProtectedRoute";
-import Home from "./pages/Home";
+import Home from "./pages/Home";  // <-- Import here
 
 export default function App() {
   return (
@@ -12,7 +12,7 @@ export default function App() {
       <Navbar />
       <div style={{ padding: "1rem" }}>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<Home />} />  {/* Updated here */}
           <Route
             path="/profile"
             element={
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,15 @@
+// src/pages/Home.jsx
+import styles from "./Home.module.css";
+import { Link } from "react-router-dom";
+
 export default function Home() {
-  return <h2>Welcome to your Social Media Feed</h2>;
+  return (
+    <div className={styles.container}>
+      <h1>Welcome to MySpaceDev</h1>
+      <p>Your personal space to learn and build awesome projects.</p>
+      <Link to="/signup" className={styles.ctaButton}>
+        Get Started
+      </Link>
+    </div>
+  );
 }
