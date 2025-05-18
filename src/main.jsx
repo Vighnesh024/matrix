69dc0f831b76aecbdcd00e7d6855f40656c3982f
Vighnesh024 <kakashikakadu024@gmail.com>
2025-05-18 15:15:32 +0530

    Remove import of missing App.css

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,9 @@
-// In src/main.jsx or src/main.js (Vite’s entry point)
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './App.css'  // <-- This line is required to import Tailwind styles
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-)
+);
